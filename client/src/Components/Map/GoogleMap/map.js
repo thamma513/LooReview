d469835c5dc17d7866090e12c8be15f6bf7512e7
Donald <donaldwhite84@gmail.com>
2018-06-13 07:58:40 -0600

    map on page

---
+++ client/src/Components/Map/GoogleMap/map.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+export class MapContainer extends Component {
+    state = {
+        selectedPlace: {
+            name: "Denver, CO"
+        }
+    }
+
+    onMarkerClick() {
+        alert("clicked");
+    }
+
+    render() {
+        return (
+            <Map 
+                google={this.props.google} 
+                zoom={14} 
+                initialCenter={{
+                    lat: 39.7392,
+                    lng: -104.9903
+                }}>
+
+                <Marker onClick={this.onMarkerClick}
+                    name={'Current location'} />
+
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
+            </Map>
+        );
+    }
+}
+
+export default GoogleApiWrapper({
+    apiKey: "YOUR-API-KEY-GOES-HERE"
+})(MapContainer);
